19d80f12a9088454e1dff62d9053a4a8654dbe73
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2025-03-28 14:52:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-import gradio as gr
+  import gradio as gr
+  import pandas as pd
 
-def filter_records(records, gender):
-    return records[records["gender"] == gender]
+  def filter_records(records, gender):
+      return records[records["gender"] == gender]
 
-demo = gr.Interface(
-    filter_records,
-    [
-        gr.Dataframe(
-            headers=["name", "age", "gender"],interactive=True,
-            datatype=["str", "number", "str"],
-            row_count=5,
-            col_count=(3, "fixed"),
-        ),
-        gr.Dropdown(["M", "F", "O"]),
-    ],
-    "dataframe",
-    description="Enter gender as 'M', 'F', or 'O' for other.",
-)
+  with gr.Blocks() as demo:
+      with gr.Row():
+          with gr.Column():
+              data_input = gr.Dataframe(
+                  headers=["name", "age", "gender"],
+                  datatype=["str", "number", "str"],
+                  row_count=5,
+                  col_count=(3, "fixed"),
+                  interactive=True
+              )
+              gender_input = gr.Dropdown(["M", "F", "O"])
+              submit_button = gr.Button("Submit")
+          with gr.Column():
+              data_output = gr.Dataframe(
+                  headers=["name", "age", "gender"],
+                  datatype=["str", "number", "str"],
+                  col_count=(3, "fixed"),
+                  interactive=True
+              )
 
-if __name__ == "__main__":
-    demo.launch(share=False, server_name="0.0.0.0", server_port=7860, ssl_verify=False)
+      submit_button.click(filter_records, inputs=[data_input, gender_input], outputs=data_output)
+
+  demo.launch(server_name="0.0.0.0", server_port=7860)
